2d12ceece1d6bc2ad2662fa9d49e52ef6bda7747
Matt Hammond <mjhammond94@gmail.com>
2023-06-21 12:44:29 +0100

    Refactor menu_item

--- lib/menu_item.rb
+++ lib/menu_item.rb
@@ -1,17 +1,6 @@
 class MenuItem
   def initialize(dish, price, type, stock_count)
-    @acceptable_types = [:starter, :dessert, :main_course]
-    if !(dish.is_a?(String)) || dish.empty?
-      fail "This is not a valid input type for dish."
-    elsif !(price.is_a?(Float)) || price < 0
-      fail "This is not a valid input for price."
-    elsif !(@acceptable_types.any? { |acceptable| acceptable == type })
-      fail "This is not a valid type of Menu Item"
-    elsif !(stock_count.is_a?(Integer)) || stock_count < 1
-      fail "This is not a valid input for stock_count"
-    else
-      @item = { dish: dish, price: price, type: type, stock_count: stock_count }
-    end
+    @item = input_validator(dish, price, type, stock_count)
   end
 
   # This method has been added just for the sake of testing
@@ -33,5 +22,20 @@ class MenuItem
 
   def dish_stock
     @item[:stock_count]
+  end
+
+  def input_validator(dish, price, type, stock_count)
+    @acceptable_types = [:starter, :dessert, :main_course]
+    if !(dish.is_a?(String)) || dish.empty?
+      fail 'This is not a valid input type for dish.'
+    elsif !(price.is_a?(Float)) || price < 0
+      fail 'This is not a valid input for price.'
+    elsif !(@acceptable_types.any? { |acceptable| acceptable == type })
+      fail 'This is not a valid type of Menu Item'
+    elsif !(stock_count.is_a?(Integer)) || stock_count < 1
+      fail 'This is not a valid input for stock_count'
+    else
+      return { dish: dish, price: price, type: type, stock_count: stock_count }
+    end
   end
 end
